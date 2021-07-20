b2ca4a3ccece6341fd9d2ded52dba1110499fea0
zyx <370349370@qq.com>
2021-07-20 14:47:46 +0800

    6

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,4 @@
-public class LinkedListDeque< T > {
+public class LinkedListDeque<T> {
     private class StuffNode {
         T first;
         StuffNode prev;
@@ -27,32 +27,32 @@ public class LinkedListDeque< T > {
         sentinel.next.prev = firstNode;
         sentinel.next = firstNode;
         sizeOfDeque++;
-     }
+    }
 
-     public void addLast(T item) {
+    public void addLast(T item) {
         StuffNode lastNode = new StuffNode(item, sentinel.prev, sentinel);
         sentinel.prev.next = lastNode;
         sentinel.prev = lastNode;
         sizeOfDeque++;
-     }
+    }
 
-     public boolean isEmpty() {
+    public boolean isEmpty() {
         return sizeOfDeque == 0;
-     }
+    }
 
-     public void printDeque() {
+    public void printDeque() {
         StuffNode iter = sentinel;
         StringBuilder builder = new StringBuilder();
-        while(iter.next != sentinel){
+        while (iter.next != sentinel) {
             builder.append(iter.next.first + " ");
             iter = iter.next;
         }
         builder.deleteCharAt(builder.length() - 1);
         System.out.println(builder.toString());
-     }
+    }
 
-     public T removeFirst() {
-        if(sizeOfDeque == 0) {
+    public T removeFirst() {
+        if (sizeOfDeque == 0) {
             return null;
         }
         StuffNode firstNode = sentinel.next;
@@ -60,10 +60,10 @@ public class LinkedListDeque< T > {
         sentinel.next.prev = sentinel;
         sizeOfDeque--;
         return firstNode.first;
-     }
+    }
 
-     public T removeLast() {
-        if(sizeOfDeque == 0){
+    public T removeLast() {
+        if (sizeOfDeque == 0) {
             return null;
         }
         StuffNode lastNode = sentinel.prev;
@@ -71,33 +71,34 @@ public class LinkedListDeque< T > {
         sentinel.prev.next = sentinel;
         sizeOfDeque--;
         return lastNode.first;
-     }
+    }
     /**
      * if index > size, then return the last element.
      */
      public T get(int index) {
         StuffNode iter = sentinel;
-        while(iter.next != sentinel) {
-           if(index == 0) {
+        while (iter.next != sentinel) {
+            if (index == 0) {
                break;
-           }
-           index--;
-           iter = iter.next;
+            }
+            index--;
+            iter = iter.next;
         }
         return iter.next.first;
      }
 
      private T getHelper(StuffNode lst, int index) {
-        if(lst.next == sentinel || index == 0){
+        if (lst.next == sentinel || index == 0) {
             return lst.next.first;
         }
         return getHelper(lst.next, index - 1);
      }
+
      public T getRecursive(int index) {
-        return getHelper(sentinel, index);
+         return getHelper(sentinel, index);
      }
 
-    private static void main(String[] args) {
+     private static void main(String[] args) {
         LinkedListDeque<Integer> testQueue = new LinkedListDeque();
         testQueue.addLast(1);
         testQueue.addFirst(2);
--- proj1a/src/ArrayDeque.java
+++ proj1a/src/ArrayDeque.java
@@ -1,4 +1,4 @@
-public class ArrayDeque< T > {
+public class ArrayDeque <T> {
     private static int mod(int x, int y) {
         return x >= 0 ? x % y : y + x % y;
     }
@@ -20,8 +20,8 @@ public class ArrayDeque< T > {
     private void enlargeByTwice() {
         int amount = items.length - firstIndex - 1;
         T[] des = (T[]) new Object[items.length * 2];
-        System.arraycopy(items, mod(firstIndex+1, items.length), des, items.length, amount);
-        System.arraycopy(items,0, des, items.length + amount, items.length - amount);
+        System.arraycopy(items, mod(firstIndex + 1, items.length), des, items.length, amount);
+        System.arraycopy(items, 0, des, items.length + amount, items.length - amount);
         firstIndex = items.length - 1;
         lastIndex = 0;
         items = des;
@@ -33,15 +33,16 @@ public class ArrayDeque< T > {
      * in both former array and present array.
      */
     private void reduceByHalf() {
-        int amount = mod(lastIndex-firstIndex, items.length) - 1;
-        T[] des = (T[]) new Object[items.length/2];
-        if(lastIndex >= firstIndex) {
-            System.arraycopy(items, mod(firstIndex + 1,items.length), des,des.length / 2, amount);
-        }
-        else {
+        int amount = mod(lastIndex - firstIndex, items.length) - 1;
+        T[] des = (T[]) new Object[items.length / 2];
+        if (lastIndex >= firstIndex) {
+            System.arraycopy(items, mod(firstIndex + 1, items.length), des, des.length / 2, amount);
+        } else {
             int firstAmount = items.length - firstIndex - 1;
-            System.arraycopy(items, mod(firstIndex + 1,items.length), des, des.length / 2, firstAmount);
-            System.arraycopy(items, 0, des, des.length / 2 + firstAmount, amount - firstAmount);
+            System.arraycopy(items, mod(firstIndex + 1, items.length), des,
+                    des.length / 2, firstAmount);
+            System.arraycopy(items, 0, des, des.length / 2 +
+                    firstAmount, amount - firstAmount);
         }
         firstIndex = des.length / 2 - 1;
         lastIndex = des.length / 2 + amount;
@@ -49,37 +50,38 @@ public class ArrayDeque< T > {
     }
 
     public void addFirst(T item) {
-        if(size == items.length) {
+        if (size == items.length) {
             enlargeByTwice();
         }
         items[firstIndex] = item;
-        firstIndex = mod(firstIndex - 1,items.length);
+        firstIndex = mod(firstIndex - 1, items.length);
         size++;
     }
 
     public void addLast(T item) {
-        if(size == items.length) {
+        if (size == items.length) {
             enlargeByTwice();
         }
         items[lastIndex] = item;
-        lastIndex = mod(lastIndex + 1,items.length);
+        lastIndex = mod(lastIndex + 1, items.length);
         size++;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     public void printDeque() {
         StringBuilder builder = new StringBuilder();
-        if(size == items.length) {
+        if (size == items.length) {
             builder.append(items[mod(firstIndex + 1, items.length)] + " ");
-            for(int i = mod(firstIndex + 2, items.length); i != lastIndex; i = mod(i + 1, items.length)){
+            for (int i = mod(firstIndex + 2, items.length);
+                i != lastIndex; i = mod(i + 1, items.length)) {
                 builder.append(items[i] + " ");
             }
-        }
-        else {
-            for(int i = mod(firstIndex + 1, items.length); i != lastIndex; i = mod(i + 1, items.length)){
+        } else {
+            for (int i = mod(firstIndex + 1, items.length);
+                 i != lastIndex; i = mod(i + 1, items.length)){
                 builder.append(items[i] + " ");
             }
         }
@@ -88,7 +90,7 @@ public class ArrayDeque< T > {
     }
 
     public T removeFirst() {
-        if(size == 0) {
+        if (size == 0) {
             return null;
         }
         int deleteIndex = mod(firstIndex + 1, items.length);
@@ -96,22 +98,22 @@ public class ArrayDeque< T > {
         firstIndex = deleteIndex;
         items[deleteIndex] = null;
         size--;
-        if(((double)size / items.length) < 0.25 && items.length > 8) {
+        if (((double) size / items.length) < 0.25 && items.length > 8) {
             reduceByHalf();
         }
         return deleteElem;
     }
 
     public T removeLast() {
-        if(size == 0) {
+        if (size == 0) {
             return null;
         }
-        int deleteIndex = mod(lastIndex - 1,items.length);
+        int deleteIndex = mod(lastIndex - 1, items.length);
         T deleteElem = items[deleteIndex];
         lastIndex = deleteIndex;
         items[deleteIndex] = null;
         size--;
-        if(((double)size / items.length) < 0.25 && items.length > 8) {
+        if (((double) size / items.length) < 0.25 && items.length > 8) {
             reduceByHalf();
         }
         return deleteElem;
@@ -123,11 +125,11 @@ public class ArrayDeque< T > {
 
     public T get(int index) {
         int i = mod(firstIndex + 1, items.length);
-        if(index > size) {
+        if (index > size) {
             return items[mod(lastIndex - 1, items.length)];
         }
-        while(true) {
-            if(index == 0){
+        while (true) {
+            if (index == 0) {
                 break;
             }
             i = mod(i + 1, items.length);
